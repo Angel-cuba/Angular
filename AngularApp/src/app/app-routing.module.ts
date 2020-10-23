3c3970efcb3a4f017edce02f9aa9fac16108a388
Angel-cuba <69958243+Angel-cuba@users.noreply.github.com>
2020-10-23 15:32:09 +0300

    Viernes_23.10_15:30

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShowComponent } from './show/show.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: '**', redirectTo: 'app-root', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+ 
+  { path: 'product-details' , component: ProductDetailsComponent },
+  { path: 'header' , component: HeaderComponent},
+  { path: 'body' , component: BodyComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'show' , component: ShowComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShowComponent } from './show/show.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailsComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    ShowComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
